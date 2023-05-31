f21d1707ca0a917ed568f5f811a71b7457004308
Michael Mooney <mikeymooney1991@gmail.com>
2023-05-31 10:12:49 -0500

    Add solve on Enter key press

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const previous = document.querySelector(".result .previous")
 const current = document.querySelector(".result .current")
 const buttons = document.querySelectorAll("button");
+const equal = document.querySelector(".equal");
 
 for(const button of buttons){
     button.addEventListener("click", (event) => {
@@ -43,10 +44,14 @@ function roundToTen(num) {
 document.body.onkeydown = (e) => {
     if(e.key === "Backspace") {
         current.textContent = current.textContent.slice(0, -1)
+    } else if (e.key === "Enter") {
+        console.log(e.key)
+        equal.click();
     }else{
         validKeys = [
             "0", "1", "2", "3", "4",
             "5", "6", "7", "8", "9"
+            , "."
         ]
         if(e.key in validKeys) {
             if(current.textContent.length < 10) current.textContent += e.key;
