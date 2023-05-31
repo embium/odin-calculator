375775d9bed6631a3b8373ee85e7fda75203865c
Michael Mooney <mikeymooney1991@gmail.com>
2023-05-30 20:17:15 -0500

    Some bug fixes

--- script.js
+++ script.js
@@ -5,21 +5,47 @@ const buttons = document.querySelectorAll("button");
 for(const button of buttons){
     button.addEventListener("click", (event) => {
         switch(event.target.textContent){
+            case "÷":
+            case "×":
+            case "-":
+            case "+":
+                let last_character = current.textContent.slice(-1);
+                if(!isNaN(parseInt(last_character)))
+                    if(current.textContent.length < 10)
+                        current.textContent += event.target.textContent;
+                break;
             case "AC":
                 current.textContent = "";
                 previous.textContent = "";
                 break;
             case "=":
+                let _previous = 0;
+                if(previous.textContent !== "") {
+                    _previous = previous.textContent;
+                }
                 let calculation = calculate(
-                    current.textContent + previous.textContent);
-                previous.textContent = calculation;
+                    current.textContent + ' + ' + _previous);
+                previous.textContent = roundToTen(calculation);
                 current.textContent = "";
                 break;
             default:
-                current.textContent += event.target.textContent;
+                if(current.textContent.length < 10)
+                    current.textContent += event.target.textContent;
                 break;
         }
     });
+}
+
+function roundToTen(num) {
+    return +(Math.round(num + "e+10")  + "e-10");
+}
+
+document.body.onkeydown = (e) => {
+    if(e.key === "Backspace") {
+        current.textContent = current.textContent.slice(0, -1)
+    }else{
+        if(current.textContent.length < 10) current.textContent += e.key;
+    }
 }
 
 function calculate(input) {
